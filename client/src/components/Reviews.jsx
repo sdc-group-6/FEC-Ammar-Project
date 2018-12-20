756aa24de532413bbf2ff11bbc3219c838aba13b
Ammar <ammar.alnatsheh@gmail.com>
2018-12-19 22:15:51 -0800

    init client basic app/ Hello World

---
+++ client/src/components/Reviews.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import $ from 'jquery';
+
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="review-main">
+       <h2>Hello World!</h2>
+      </div>
+    );
+  }
+}
+
+export default Review;
