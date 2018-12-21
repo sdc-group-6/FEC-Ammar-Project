622793fa7f07e0f8ed06697d0cba4ac826deb048
Ammar <ammar.alnatsheh@gmail.com>
2018-12-20 23:45:38 -0800

    build the router for get reviews by resturant id

--- server/index.js
+++ server/index.js
@@ -17,7 +17,18 @@ app.get('/reviews',(req,res) => {
       res.status(501).send(err);
     } else {
       res.status(200).send(data);
-    }
+    } 
+  });
+});
+
+app.get('/reviews/:id',(req,res) => {
+  // on reviews router get all reviews from the database
+  db.getReviewsById( req.params.id, (err,data) => {
+    if (err) {
+      res.status(501).send(err);
+    } else {
+      res.status(200).send(data);
+    } 
   });
 });
 
