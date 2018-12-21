948ab614d7f01a2531425b8222c8f3a0de0a8b3f
Ammar <ammar.alnatsheh@gmail.com>
2018-12-21 11:20:43 -0800

    use path to fix the static middleware serveing assets path

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const db = require('../database/db_modules');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '../client/dist'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/reviews',(req,res) => {
   // on reviews router get all reviews from the database
