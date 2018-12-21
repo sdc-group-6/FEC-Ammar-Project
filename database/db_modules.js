62869fe62e67e15ba265ba0ae3152bf9b686d573
Ammar <ammar.alnatsheh@gmail.com>
2018-12-20 23:01:50 -0800

    build get reviews by resturant by id

--- database/db_modules.js
+++ database/db_modules.js
@@ -11,6 +11,11 @@ const getAllReviews = function(callback) {
 
 const getReviewsById = function(id,callback) {
     // get All the reviews from the reviws database by a resturant id
+    db.query(`SELECT reviews.id, users.name, restaurants.name, content, rating, delivery_rating, order_rating, update_date 
+     FROM reviews 
+     INNER JOIN users ON reviews.user_id = users.id 
+     INNER JOIN restaurants ON reviews.restaurant_id = restaurants.id
+     WHERE reviews.restaurant_id = ${id}`,callback);
 
 };
 
