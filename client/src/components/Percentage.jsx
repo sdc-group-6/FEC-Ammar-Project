ad8a108b4aafec18d3177ce6f589ad725643364e
Ammar <ammar.alnatsheh@gmail.com>
2018-12-22 15:20:51 -0800

    init percentage component

---
+++ client/src/components/Percentage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Percentage = (props) => (
+  <div>
+    <h1>Percentage</h1>
+      <div className="percentage">
+      </div>
+  </div>
+);
+
+export default Percentage;
