7aabc6174f173ff0bebd76090e498b2b08417017
Ammar <ammar.alnatsheh@gmail.com>
2018-12-21 19:08:19 -0800

    fetching data from the server using ip

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -5,16 +5,61 @@ class Review extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      reviews: [],
+      restaurant_name: '',
+      reviews_number: 0,
+      rating: 0,
+      delivery_rating: 0,
+      order_rating: 0
+    }
+
   }
 
   componentDidMount() {
-    console.log("we got the rest_id",this.props.restaurant_id);
+
+    fetch(`reviews/${this.props.restaurant_id}`)
+    .then(response => response.json())
+    .then(data => {
+
+      let restaurant_name = data[0]['restaurant_name'] || '';
+      let reviews_number = data.length || 0;
+      let rating = 0;
+      let delivery_rating = 0;
+      let order_rating = 0;
+
+      // calculate the summation of rating, delivery_rating and order_rating
+      data.forEach(review => {
+        rating += review['rating'] || 0;
+        delivery_rating += review['delivery_rating'] || 0;
+        order_rating += review['order_rating'] || 0;
+      });
+
+      this.setState({
+        reviews: data,
+        restaurant_name: restaurant_name,
+        reviews_number: reviews_number,
+        rating: rating,
+        delivery_rating: delivery_rating,
+        order_rating: order_rating
+      });
+
+    })
+    .catch(function(error) {
+      console.log('Cant get the reviews from the server', error);
+    });
+
   }
 
   render() {
     return (
       <div className="review-main">
-       <h2>Hello World!</h2>
+       <h2>Hello World</h2>
+       <h5>{this.state.restaurant_name}</h5>
+       <h5>{this.state.reviews_number}</h5>
+       <h5>{this.state.rating}</h5>
+       <h5>{this.state.delivery_rating}</h5>
+       <h5>{this.state.order_rating}</h5>
       </div>
     );
   }
