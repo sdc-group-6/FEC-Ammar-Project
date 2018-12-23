edb20be52464becd2692da8c86717f546d5ebb28
Ammar <ammar.alnatsheh@gmail.com>
2018-12-23 00:12:50 -0800

    implement the stars bar using awsome-font

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Stars = (props) => (
-  <div>
-      <div className="starts">
-      starts filling percentage {Math.round((props.rate/props.reviews_number)*20)}
-      </div>
-  </div>
-);
+const Stars = (props) => {
+    let starts_filling_percentage = Math.round((props.rate/props.reviews_number)*20);
+  return (
+  <div className="stars">
+  { starts_filling_percentage >= 10? <span className="fa fa-star checked"></span> : <span className="fa fa-star not-checked"></span>}
+  { starts_filling_percentage >= 30? <span className="fa fa-star checked"></span> : <span className="fa fa-star not-checked"></span>}
+  { starts_filling_percentage >= 50? <span className="fa fa-star checked"></span> : <span className="fa fa-star not-checked"></span>}
+  { starts_filling_percentage >= 70? <span className="fa fa-star checked"></span> : <span className="fa fa-star not-checked"></span>}
+  { starts_filling_percentage >= 90? <span className="fa fa-star checked"></span> : <span className="fa fa-star not-checked"></span>}
+   </div>
+  );
+};
 
 export default Stars;
