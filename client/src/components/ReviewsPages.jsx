407bbbcb3a4de7fe70add7812860edf28168de07
Ammar <ammar.alnatsheh@gmail.com>
2018-12-24 17:12:46 -0800

    switching to Inline styling/ refactor all components

--- client/src/components/ReviewsPages.jsx
+++ client/src/components/ReviewsPages.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import style from '../style/style.css.jsx';
 
 const ReviewsPages = (props) => {
     let pages = [];
     let counter = 1;
     for (var i = 1; i <= props.reviews_number; i+=10) {
-        pages.push(<span className={counter} key={i} id="link" onClick={props.changeReviewsPage}>{counter}</span>);
+        pages.push(<span className={counter} key={i} id="link" style={style.link} onClick={props.changeReviewsPage}>{counter}</span>);
         counter++;
     }
     
   return (
-  <div className="reviews-list-footer">
-    <span className="-1" id="link" key="-1" onClick={props.changeReviewsPage}>Prev</span>
+  <div className="reviews-list-footer" style={style.reviewsListFooter}>
+    <span className="-1" id="link" style={style.link} key="-1" onClick={props.changeReviewsPage}>Prev</span>
     {pages}
-    <span className="0" id="link" key="0" onClick={props.changeReviewsPage}>Next</span>
+    <span className="0" id="link" style={style.link} key="0" onClick={props.changeReviewsPage}>Next</span>
   </div>
   );
 };
---
+++ client/src/style/style.css.jsx
@@ -0,0 +1,71 @@
+const reviewMain = {
+  padding: '30px',
+};
+
+const percentageList = {
+  paddingTop: '1px',
+  marginTop: '1px',
+  display: 'inline-flex',
+};
+
+const percentage = {
+  padding: '10px',
+  maxWidth: '65px',
+};
+
+const rating = {
+  marginTop: '1px',
+  fontSize: '13px',
+  color: 'gray',
+};
+
+const percentageText = {
+  fontSize: '13px',
+  color: 'gray',
+};
+
+const fa = {
+  fontSize: '15px',
+};
+
+const checked = {
+  color: 'gold',
+};
+
+const notChecked = {
+  color: 'lightgray',
+};
+
+const reviewsListSorting = {
+  fontSize: '17px',
+};
+
+const reviewsListHeader = {
+  float: 'right',
+};
+
+const reviewsListFooter = {
+  textAlign: 'center',
+};
+
+const link = {
+  color: 'blue',
+  textDecoration: 'underline',
+  padding: '5px',
+  cursor: 'pointer',
+};
+
+export default {
+  reviewMain,
+  percentageList,
+  percentage,
+  rating,
+  percentageText,
+  fa,
+  checked,
+  notChecked,
+  reviewsListSorting,
+  reviewsListHeader,
+  reviewsListFooter,
+  link,
+};
