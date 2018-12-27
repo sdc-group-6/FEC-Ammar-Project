2c69ae2142d4a993e8c11d381d824d274465d4cb
Ammar <ammar.alnatsheh@gmail.com>
2018-12-27 13:30:29 -0800

    clean the code airbnb style

--- server/index.js
+++ server/index.js
@@ -1,35 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const db = require('../database/db_modules');
 const path = require('path');
+const db = require('../database/db_modules');
+
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(morgan('combined'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/reviews',(req,res) => {
+app.get('/reviews', (req, res) => {
   // on reviews router get all reviews from the database
-  db.getAllReviews((err,data) => {
+  db.getAllReviews((err, data) => {
     if (err) {
       res.status(501).send(err);
     } else {
       res.status(200).send(data);
-    } 
+    }
   });
 });
 
-app.get('/reviews/:id',(req,res) => {
+app.get('/reviews/:id', (req, res) => {
   // on reviews router get all reviews from the database
-  db.getReviewsByRestaurantId( req.params.id, (err,data) => {
+  db.getReviewsByRestaurantId(req.params.id, (err, data) => {
     if (err) {
       res.status(501).send(err);
     } else {
       res.status(200).send(data);
-    } 
+    }
   });
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     loaders: [
@@ -15,9 +16,9 @@ module.exports = {
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        },
+      },
+    ],
+  },
 };
