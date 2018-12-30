474af74033bd98f56246427e3ba10a262941856f
Ammar <ammar.alnatsheh@gmail.com>
2018-12-30 11:06:05 -0800

    add the menu and order_items option

--- database/db_modules.js
+++ database/db_modules.js
@@ -2,7 +2,7 @@ const db = require('../database');
 
 function getAllReviews(callback) {
   // get All the reviews from the reviws database
-  db.query(`SELECT reviews.id, users.name AS user_name, restaurants.name AS restaurant_name, content, rating, delivery_rating, order_rating, update_date 
+  db.query(`SELECT reviews.id, users.name AS user_name, restaurants.name AS restaurant_name, restaurants.menu, content, order_items, rating, delivery_rating, order_rating, update_date 
      FROM reviews 
      INNER JOIN users ON reviews.user_id = users.id 
      INNER JOIN restaurants ON reviews.restaurant_id = restaurants.id
@@ -11,7 +11,7 @@ function getAllReviews(callback) {
 
 function getReviewsByRestaurantId(id, callback) {
   // get All the reviews from the reviws database by a resturant id
-  db.query(`SELECT reviews.id, users.name AS user_name, restaurants.name AS restaurant_name, content, rating, delivery_rating, order_rating, update_date 
+  db.query(`SELECT reviews.id, users.name AS user_name, restaurants.name AS restaurant_name, restaurants.menu, content, order_items, rating, delivery_rating, order_rating, update_date 
     FROM reviews 
     INNER JOIN users ON reviews.user_id = users.id 
     INNER JOIN restaurants ON reviews.restaurant_id = restaurants.id
