82140990b70133f3f347e2c16858186873e1a479
Ammar <ammar.alnatsheh@gmail.com>
2018-12-30 16:39:48 -0800

    switch to react class component style

--- client/src/components/OrderListEntity.jsx
+++ client/src/components/OrderListEntity.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import style from '../style/style.css.jsx';
 
-const OrderListEntity = (props) => {
-  return (
-  <div className="order-list-entity" style={style.orderListEntity}>
-    {props.item['name'] + ' + '}
-  </div>
-  );
+class OrderListEntity extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        value: ''
+      };
+    }
+    
+    render() {
+        return (
+        <div className="order-list-entity" style={style.orderListEntity}>
+            {this.props.item['name'] + ' + '}
+        </div>
+        );
+    }
 };
 
 export default OrderListEntity;
