44e349b2f38e917d5420d29c9cfb0a8dee6580de
Ammar <ammar.alnatsheh@gmail.com>
2019-01-03 12:03:31 -0800

    have reviews component ready fora proxy server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './Components/Reviews.jsx';
 
-ReactDOM.render(<Reviews />, document.getElementById('root') || document.createElement('div') // for testing purposes
+ReactDOM.render(<Reviews />, document.getElementById('Reviews') || document.createElement('div') // for testing purposes
 );
